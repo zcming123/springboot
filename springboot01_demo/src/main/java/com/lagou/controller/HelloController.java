1309b785744ca0a8d88c8ef998d4af2627db2f8f
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-09-11 12:10:13 +0800

    springboot基础回顾

---
+++ springboot01_demo/src/main/java/com/lagou/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.lagou.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //相当于配置 @Responsebody+@Controller
+public class HelloController {
+    @RequestMapping("/demo")
+    public String demo() {
+        return "你好，springboot";
+    }
+}
