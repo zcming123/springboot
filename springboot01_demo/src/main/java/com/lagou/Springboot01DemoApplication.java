3686f20234058f4b1b4afae7e2cd318789ace255
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-09-11 16:14:54 +0800

    Update Springboot01DemoApplication.java

--- springboot01_demo/src/main/java/com/lagou/Springboot01DemoApplication.java
+++ springboot01_demo/src/main/java/com/lagou/Springboot01DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication // 能够扫描 Spring 组件并自动配置 Spring Boot
 public class Springboot01DemoApplication {
 
     public static void main(String[] args) {
