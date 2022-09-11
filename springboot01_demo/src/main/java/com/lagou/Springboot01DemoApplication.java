b975bdaf4d42eef3f31659487180c8a6d9d54c88
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-09-11 22:26:10 +0800

    Update Springboot01DemoApplication.java

--- springboot01_demo/src/main/java/com/lagou/Springboot01DemoApplication.java
+++ springboot01_demo/src/main/java/com/lagou/Springboot01DemoApplication.java
@@ -3,7 +3,8 @@ package com.lagou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// 项目启动类是一个配置类。并可以被组件扫描器扫描，
+// 因为 @SpringBootApplication中的 @SpringBootConfiguration 上有 @Configuration。
 @SpringBootApplication // 能够扫描 Spring 组件并自动配置 Spring Boot
 public class Springboot01DemoApplication {
 
